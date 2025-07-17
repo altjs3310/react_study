225a143e3f9c925e17dc36d3d6f23f266fdd25e9
altjs3310 <altjs3310@naver.com>
2025-07-17 12:07:29 +0900

    detail 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,11 @@ function App() {
   // useXXX : 리액트 내장함수 (리액트 훅)
   const [title, setTitle] = useState('상품목록');
   const [boardTitle, setBoardTitle] = useState(['React', 'HTML', 'CSS']);
-  const [like, setLike] = useState(0);
+  const [like, setLike] = useState([0, 0, 0]);
   const [show, setShow] = useState(false);
+  const [titleIndex, setTitleIndex] = useState(0); // 몇번째 게시글을 클릭한지 저장
 
+  let arr = [1,2,3,4,5];
 
   function change() {
     setLike(like + 1);
@@ -17,7 +19,16 @@ function App() {
 
   return (
    <div className='App'>
-
+    {/* {
+      arr.map((data) => {
+        return (
+          <>
+          <div>{data}</div>
+          <p>ㅇㄻㄹㅇ</p>
+          </>
+        )
+      })
+    } */}
     <div className='nav'>
       <h3>{title}</h3>
     </div>
@@ -26,20 +37,38 @@ function App() {
       setTitle('게시판');
     }}>제목바꾸기</button>
 
-    <div className="list">
+    {
+      boardTitle.map((title, i) => {
+        return(
+          <div className="list" key={i}>
+            <h4 onClick={() =>{
+              setShow(!show);  
+              setTitleIndex(i);            
+            }}>{title}<button onClick={(e) => {
+              e.stopPropagation();
+              let _like = [...like];
+              _like[i] = _like[i] + 1;
+              setLike(_like);
+            }}>좋아요</button>{like[i]}</h4>
+            <p>2025-07-16</p>
+          </div>
+        )
+      })
+    }
+
+    {/* <div className="list">
       <h4>{boardTitle[0]}<button onClick={change}>좋아요</button> {like} </h4>
       <p>2025-07-16</p>
-    </div>
+    </div> */}
 
-    <div className="list">
-      <h4>{boardTitle[1]}</h4>
-      <p>2025-07-16</p>
-    </div>
+   
 
-    <div className="list">
-      <h4>{boardTitle[2]}</h4>
+    {/* <div className="list">
+      <h4 onClick={ () => {
+      setShow(!show);}}>
+      {boardTitle[2]}</h4>
       <p>2025-07-16</p>
-    </div>
+    </div> */}
 
     <button onClick={ () => {
       let _boardTitle = [...boardTitle];
@@ -48,12 +77,8 @@ function App() {
     }}>첫번째 게시물 제목바꾸기</button>
 
     {
-      show ? <Detail/> : ''
+      show ? <Detail boardTitle={boardTitle} setBoardTitle={setBoardTitle} titleIndex={titleIndex}/> : ''
     }
-    
-    <button onClick={ () => {
-      setShow(true);
-    }}>버튼</button>
 
    </div>
   )
--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,7 +1,26 @@
-function Detail() {
+// function Detail(props) {
+//   return (
+//     <div className="detail">
+//       <button onClick={ () => {
+//         let _boardTitle = [...props.boardTitle];
+//         _boardTitle[0] = "ddd";
+//         props.setBoardTitle(_boardTitle);
+//       }}>제목바꿔주는 버튼</button>
+//       <h4>{props.boardTitle[props.titleIndex]}</h4>
+//       <p>날짜</p>
+//       <p>내용</p>  
+//     </div> 
+//   )
+// }
+function Detail({boardTitle, setBoardTitle, titleIndex}) {
   return (
     <div className="detail">
-      <h4>글제목</h4>
+      <button onClick={ () => {
+        let _boardTitle = [...boardTitle];
+        _boardTitle[0] = "ddd";
+        setBoardTitle(_boardTitle);
+      }}>제목바꿔주는 버튼</button>
+      <h4>{boardTitle[titleIndex]}</h4>
       <p>날짜</p>
       <p>내용</p>  
     </div> 
